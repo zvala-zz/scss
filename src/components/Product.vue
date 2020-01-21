6482f29c9eef8c6026a228e283a2634e5f617203
Cesar Zavala V <zvala93@gmail.com>
2020-01-20 21:57:39 -0500

    Se agrega componente producto.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="product" :class="{'product--dark': discount}">
-        <img :src="url" alt="" class="product__image">
-        <p class="product__title">{{title}}</p>
-        <span class="product__price">{{price}}</span>
-    </div>
+  <div class="product">
+    <span class="product__price">{{price}}</span>
+    <img :src="img" alt class="product__image" />
+    <p class="product__title">{{name}}</p>
+    <span>{{color}}</span>
+    <p>{{description}}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'product',
-    props: {
-        url: {
-            type: String
-        },
-        title: {
-            type: String
-        },
-        price: {
-            type: Number
-        },
-        discount: Boolean
-    }
-}
+  name: "product",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: String,
+    price: Number,
+    color: String,
+    img: String,
+    description: String,
+    category: String
+  }
+};
 </script>
