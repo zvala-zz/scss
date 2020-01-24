e9e448497668a3b8cb386ed14ce9139b654cc67c
Cesar Zavala V <zvala93@gmail.com>
2020-01-23 22:17:16 -0500

    Se estructura mejor el codigo.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="product-list">
-    <product v-for="(item, index) in products" :key="index" :id="item.id" :name="item.name" :price="item.price" :img="item.img" :color="item.color" :description="item.description" :category="item.category"/>
+    <product
+      v-for="(item, index) in products"
+      :key="index"
+      :id="item.id"
+      :name="item.name"
+      :price="item.price"
+      :img="item.img"
+      :color="item.color"
+      :description="item.description"
+      :category="item.category"
+      :discount="item.discount"
+    />
   </div>
 </template>
 
